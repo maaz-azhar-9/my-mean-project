96172c9191916d8b07e819b71dbc5069dfd9cdcb
Mohd Maaz Azhar <maazazhar9@gmail.com>
2025-07-19 12:49:50 +0530

    feat: API to add user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,30 @@
 const express = require("express");
 
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
 
-router.post('/signup',(req, res)=>{
+const User = require('../models/user')
+
+router.post('/signup',(req, res) => {
+
+    bcrypt.hash(req.body.password, 10).then((hash) => {
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        })
+
+        user.save().then((result) => {
+            res.status(201).json({
+                message: "User successfully created",
+                result: result
+            })
+        }).catch((err) => {
+            res.status(500).json({
+                error:err
+            })
+        })
+    })
 
 })
 
