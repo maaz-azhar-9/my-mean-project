8ffa44713151025e88085972520c7a05f44a7435
Mohd Maaz Azhar <maazazhar9@gmail.com>
2025-08-07 02:08:43 +0530

    fix: showing not authorized dialog, when not changing anything and saving in edit mode

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -63,7 +63,8 @@ exports.updatePost = (req, res, next) => {
     })
 
     Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then((result) => {
-        if (result.modifiedCount > 0) {
+        console.log(result)
+        if (result.matchedCount > 0) {
             res.status(200).json({
                 message: "Successfully Updated"
             })
