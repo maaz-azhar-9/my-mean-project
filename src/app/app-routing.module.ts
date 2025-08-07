765f693db56711cdfac22f63a7c20f016935ba98
Mohd Maaz Azhar <maazazhar9@gmail.com>
2025-08-08 03:29:20 +0530

    refactor: added lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
 import { CreatePostComponent } from './posts/create-post.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { authGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {path:'', component:PostListComponent},
   {path:'create', component:CreatePostComponent, canActivate:[authGuard]},
   {path:'edit/:postId', component:CreatePostComponent, canActivate:[authGuard]},
-  {path:'login', component:LoginComponent},
-  {path:'signup', component:SignupComponent},
+  {path:"auth", loadChildren: () => import("./auth/auth-routing.module").then(m => m.AuthRoutingModule)}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ErrorComponent } from './error/error.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +30,7 @@ import { AuthModule } from './auth/auth.module';
     AngularMaterialModule,
     PostsModule,
     AuthModule,
+    AuthRoutingModule,
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true},
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true}
