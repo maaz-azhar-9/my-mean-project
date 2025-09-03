de64c76fdc6057269714de48322ad79afd1476bf
Mohd Maaz Azhar <maazazhar9@gmail.com>
2025-09-03 18:42:09 +0530

    feat: added trnsaction in like/dislike to avoid data inconsistency

--- backend/controllers/likes.js
+++ backend/controllers/likes.js
@@ -1,43 +1,80 @@
+const { mongoose } = require('mongoose');
 const Like = require('../models/likes');
 
 const Post = require('../models/post')
 
-exports.like = (req,res,next) => {
-    const postId = req?.params?.postId;
-    const userId = req?.params?.userId;
-    const like = new Like({
-        postId: postId,
-        userId: userId
-    });
-
-    like.save().then(()=>{
-        Post.updateOne({_id:postId},{$inc:{likeCount:1}}).then((response)=>{
-            response.modifiedCount ? res.json({message:"Liked"}) : res.json({message:"Post not found."})
-        })
-    }).catch(() => {
-        res.status(500).json({
-            message:"Unkown error"
-        })
-    })
-}
-
-exports.removeLike = (req,res,next) =>{
-    const postId = req?.params?.postId;
-    const userId = req?.params?.userId;
-    Post.updateOne({_id: postId, likeCount: { $gt: 0 }},{$inc:{likeCount:-1}}).then((response)=>{
-        if(response.modifiedCount){
-        Like.deleteOne({postId: postId, userId: userId}).then(()=>{
-            res.status(200).json({
-                message: "Unliked"
+exports.like = (req, res, next) => {
+    const { postId, userId } = req.params;
+    const like = new Like({ postId, userId });
+  
+    mongoose.startSession()
+      .then((session) => {
+        session.startTransaction();
+  
+        return like.save({ session })
+          .then(() => {
+            return Post.updateOne(
+              { _id: postId },
+              { $inc: { likeCount: 1 } },
+              { session }
+            );
+          })
+          .then((response) => {
+            if (!response.modifiedCount) {
+              throw new Error("Post not found");
+            }
+            return session.commitTransaction().then(() => {
+              res.json({ message: "Liked" });
             });
-        }).catch(() => {
-            res.status(500).json({
-                message: "Unknown error"
+          })
+          .catch((err) => {
+            return session.abortTransaction().then(() => {
+              res.status(500).json({ message: err.message || "Unknown error" });
             });
+          })
+          .finally(() => {
+            session.endSession();
+          });
+      });
+  };
+  
+
+  exports.removeLike = (req, res, next) => {
+    const { postId, userId } = req.params;
+  
+    mongoose.startSession()
+      .then((session) => {
+        session.startTransaction();
+  
+        return Post.updateOne(
+          { _id: postId, likeCount: { $gt: 0 } },
+          { $inc: { likeCount: -1 } },
+          { session }
+        )
+        .then((response) => {
+          if (!response.modifiedCount) {
+            throw new Error("No post found to unlike");
+          }
+          
+          return Like.deleteOne({ postId, userId }, { session });
         })
-    }
-    else{
-        res.json({message:"No post found to unlike"});
-    }
-    })
-}
+        .then((deleteResponse) => {
+          if (!deleteResponse.deletedCount) {
+            throw new Error("Like record not found");
+          }
+  
+          return session.commitTransaction().then(() => {
+            res.status(200).json({ message: "Unliked" });
+          });
+        })
+        .catch((err) => {
+          return session.abortTransaction().then(() => {
+            res.status(500).json({ message: err.message || "Unknown error" });
+          });
+        })
+        .finally(() => {
+          session.endSession();
+        });
+      });
+  };
+  
